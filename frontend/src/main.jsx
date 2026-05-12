b0058a031aa5c9c211f77fd6503e9625195cf46b
DajkaBenceImre <dajkabenceimre@gmail.com>
2026-05-12 05:22:52 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'; // <-- ÚJ IMPORT
-import { store } from './store/store'; // <-- ÚJ IMPORT
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 import App from './App';
 import { ModalProvider } from './context/ModalContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}> {/* <-- PROVIDER HOZZÁADÁSA */}
+    <Provider store={store}>
       <BrowserRouter>
         <ModalProvider>
           <App />
